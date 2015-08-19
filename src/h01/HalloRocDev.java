974a8979919dcba32f3952bfd6d80a616f928cd8
Soufyane <Soufyane@10.69.12.64>
2015-08-19 14:34:35 +0200

    Ik heb er een string bij toegevoegd

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -4,6 +4,7 @@ public class HalloRocDev {
 
 	public static void main(String[] args) {
 		System.out.println("Hallo roc-dev!");
+		System.out.println("Hallo ik ben soufyane en heb git geinstalleerd.");
 	}
 
 }
