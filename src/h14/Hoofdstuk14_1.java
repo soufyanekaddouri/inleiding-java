06a88459b3f7faf77c1020ffd3c8dbed41655b05
Soufyane <Soufyane@178.84.73.50>
2015-11-25 20:58:07 +0100

    Hoofdstuk14

--- src/h14/Hoofdstuk14_1.java
+++ src/h14/Hoofdstuk14_1.java
@@ -2,29 +2,26 @@ package h14;
 
 import java.applet.Applet;
 import java.awt.Graphics;
+import java.util.Random;
 
 public class Hoofdstuk14_1 extends Applet{
 	private static final long serialVersionUID = 1L;
 
 	String[] kleuren = {"Harten", "Ruiten", "Schoppen", "Klaver"};
-	String[] kaarten = {"twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "tien", "boer", "vrouw", "heer", "aas"};
-	
-	Double kleur;
-	Double kaart;
-	
+	String[] kaarten = {"twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "tien", "Boer", "Vrouw", "Heer", "Aas"};
+	String text = "";
 	
 	public void init() {
-		setSize(200,200);
-		
-		
-		
-		
+		setSize(250, 200);
+		int  random  = new Random().nextInt(kleuren.length);
+		int  random2 = new Random().nextInt(kaarten.length);
+		text = "U heeft de kleur " + kleuren[random]+" en "+kaarten[random2];
 	}
 	
 	public void paint(Graphics g) {
+		g.drawString(text, 20, 50);
 		
 	}
 	
-	
-	
+
 }
--- src/h14/Hoofdstuk14_2.java
+++ src/h14/Hoofdstuk14_2.java
@@ -1,6 +1,8 @@
 package h14;
 
 import java.applet.Applet;
+import java.awt.Graphics;
+import java.util.Random;
 
 public class Hoofdstuk14_2 extends Applet{
 	private static final long serialVersionUID = 1L;
@@ -15,12 +17,33 @@ public class Hoofdstuk14_2 extends Applet{
 	String[] speler3 = new String[13];
 	String[] speler4 = new String[13];
 	
+	String randomkaart;
+	
+	int y;
+	
 	public void init() {
 		setSize(800, 500);
+		int random = new Random().nextInt(kleuren.length);
+		int random2 = new Random().nextInt(kaarten.length);
+		
+		randomkaart = "" + kleuren[random] + " " + kaarten[random2];
+		
+		y = 60;
 		
 		//DECK VULLEN MET KAARTEN
+	}
+	
+	public void paint(Graphics g) {
+		g.drawString("Speler 1", 100, 20);
+		g.drawString(""+randomkaart, 100, y);
 		
+		g.drawString("Speler 2", 300, 20);
+		g.drawString("Speler 3", 500, 20);
+		g.drawString("Speler 4", 700, 20);
 		
+		while (y < 260) {
+			y++;
+		}
 	}
 	
 	
